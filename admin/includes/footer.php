398fe79f8f324cd1b7c66d87b5fd614dca97c4a6
DCtron <deepakchoudhary1026@gmail.com>
2021-06-16 15:08:28 +0530

    commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+            <div class="footer-area">
+                <p>Designed by DEEPAK CHOUDHARY &copy;2021. All rights reserved </p>
+            </div>
+        </footer>
---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<div class="footer-section">
+				<div class="container">
+					<div class="footer-top">
+						<p>Designed by DEEPAK CHOUDHARY &copy;2021. All rights reserved </p>
+					</div>
+				</div>
+			</div>
